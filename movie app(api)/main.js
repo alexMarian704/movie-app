d2132b97d12cf5765c2c41350e98be66bdb8c248
Alex <77676243+alexMarian704@users.noreply.github.com>
2021-01-26 21:54:26 +0200

    Update main.js

--- movie app(api)/main.js
+++ movie app(api)/main.js
@@ -1,6 +1,6 @@
-const API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=";
 const IMAGE_API = "https://image.tmdb.org/t/p/w500";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=";
 
 const Body = document.getElementById('main');
 const search = document.getElementById('searchMovie');
